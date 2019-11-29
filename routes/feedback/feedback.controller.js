08b50311078c40d8ecd71b1641b4b0679d7d7bcd
smcmurtry <smcmurtry@gmail.com>
2019-11-29 15:13:58 -0700

    trying out another way of including the plugin js through webpack

--- routes/feedback/feedback.controller.js
+++ routes/feedback/feedback.controller.js
@@ -1,11 +1,9 @@
-const { routeUtils, getClientJs } = require('./../../utils')
+const { routeUtils } = require('./../../utils')
 
 module.exports = (app, route) => {
 
   route.draw(app)
     .get((req, res) => {
-      const js = getClientJs(req, "inlineFeedback")
-
-      res.render("feedback-plugin", routeUtils.getViewData(req, { pluginJsFiles: js ? [js] : false }))
+      res.render("feedback-plugin", routeUtils.getViewData(req))
     })
 }
--- utils/data.helpers.js
+++ utils/data.helpers.js
@@ -1,9 +1,13 @@
 const { getSessionData } = require('./session.helpers')
 const { getFlashMessage } = require('./flash.message.helpers')
+const { getClientJs } = require('./load.helpers')
 
 const getViewData = (req, optionalParams = {}) => {
+  const js = getClientJs(req, "inlineFeedback")
+
   const params = {
     data: { ...getSessionData(req) },
+    pluginJsFiles: js ? [js] : false,
   }
 
   const errors = getFlashMessage(req)
