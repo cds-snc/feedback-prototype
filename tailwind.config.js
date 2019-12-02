960c1d1c9a002e26938a507dcda681f48fdd55d2
smcmurtry <smcmurtry@gmail.com>
2019-12-02 15:34:56 -0700

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   theme: {
     extend: {},
+    colors: {
+      blue: {
+        dark: "#284162",
+        light: "#335075",
+      },
+    },
   },
   variants: {},
   plugins: [],
