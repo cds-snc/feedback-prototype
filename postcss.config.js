6ba5086b4775243f78ebd09c5507031b387c8448
smcmurtry <smcmurtry@gmail.com>
2019-12-03 07:59:24 -0700

    removed purgecss, which was breaking styling of inline-feedback in production

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,4 @@
 const cssnano = require('cssnano')
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  // Specify the paths to all of the template files in your project
-  content: ['./routes/**/*.njk', './views/**/*.njk'],
-
-  // Include any special characters you're using in this regular expression
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-})
 
 module.exports = {
   plugins: [
@@ -15,6 +8,6 @@ module.exports = {
       preset: 'default',
     }),
     require('autoprefixer'),
-    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+    ...([]),
   ],
 }
